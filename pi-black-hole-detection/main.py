ce8d51939ca77de8afe262fe49d702e6bd5e5fa2
Soraaot Channon <116389026+TxngJr@users.noreply.github.com>
2023-12-08 20:36:07 +0700

    v0.0.9

--- pi-black-hole-detection/main.py
+++ pi-black-hole-detection/main.py
@@ -4,7 +4,7 @@ import cv2
 import requests
 import os
 
-url = 'http://192.168.1.45:3000/api/upload-black-hole'
+url = 'http://gps.utc.ac.th:3000/api/upload-black-hole'
 folder_path = "img/"
 
 gps = serial.Serial("/dev/ttyS0", baudrate=9600)
@@ -16,7 +16,7 @@ is_hold_detect = False
 
 lat = 0.0
 lon = 0.0
-
+hold = 0
 
 def read_gps():
     global lat, lon
@@ -34,20 +34,6 @@ gps_thread.daemon = True
 gps_thread.start()
 
 while True:
-    ret, frame = cap.read()
-    if ret:
-        hold = cascade_hold.detectMultiScale(frame)
-        for (x, y, w, h) in hold:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.imshow('Webcam', frame)
-        if len(hold) > 0:
-            if not is_hold_detect:
-                is_hold_detect = True
-                filename = f"{folder_path}{lat}, {lon}.jpg"
-                cv2.imwrite(filename, frame)
-        else:
-            is_hold_detect = False
-
     key = cv2.waitKey(1) & 0xFF
     if key == 27:
         break
@@ -58,12 +44,30 @@ while True:
             image_path = os.path.join(folder_path, filename)
             with open(image_path, 'rb') as file:
                 files = {'image': (filename, file, 'image/jpeg')}
-                response = requests.post(url, files=files)
-                if response.status_code == 201:
-                    os.remove(image_path)
-                    print('File uploaded successfully!')
-                else:
-                    print(f'Failed to upload file. Status code: {response.status_code}')
-
+                try:
+                    response = requests.post(url, files=files)
+                    if response.status_code == 201:
+                        os.remove(image_path)
+                        print('File uploaded successfully!')
+                    else:
+                        print(f'Failed to upload file. Status code: {response.status_code}')
+                except:
+                    print("error")
+    ret, frame = cap.read()
+    if ret:
+        hold = cascade_hold.detectMultiScale(frame)
+        if key == 115 or key == 83:
+            hold = [[0,0,0,0]]
+        for (x, y, w, h) in hold:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.imshow('Webcam', frame)
+        if len(hold) > 0:
+            if not is_hold_detect:
+                is_hold_detect = True
+                filename = f"{folder_path}{lat}, {lon}.jpg"
+                cv2.imwrite(filename, frame)
+        else:
+            is_hold_detect = False
+             
 cap.release()
 cv2.destroyAllWindows()
