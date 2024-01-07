3246530e712644cb6b0669ceb7e272b7350557f7
Denmann N'ramzz <116389026+TxngJr@users.noreply.github.com>
2024-01-07 14:31:30 +0700

    Update constants.ts

--- web/Frontend/street-sight-ai-navigator/src/constants.ts
+++ web/Frontend/street-sight-ai-navigator/src/constants.ts
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = 'http://110.164.222.7:3000';
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'http://110.164.222.7:3000';
+// export const API_BASE_URL = 'http://localhost:3000';
 export const GOOGLE_MAP_API_KEY = 'AIzaSyAYo9E_FaMLIjTtPbqO4UGCcCgJm9P3xc0';
